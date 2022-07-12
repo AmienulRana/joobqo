9c518781402411ffe08aae2339a3d31defffadda
AmienulRana <amienulrana@gmail.com>
2022-07-12 22:40:51 +0700

    mengubah styling module => tailwind css

--- components/element/button/button.js
+++ components/element/button/button.js
@@ -1,15 +1,19 @@
 import styles from './button.module.scss';
 import PropTypes from 'prop-types';
 export default function Button({ children, classNameProps, width, background, onClick }){
-    const newClass = [styles.button, styles[background], classNameProps].join(' ');
+    const newClass = ['outline-0 border-0 font-bold h-30px rounded-md text-white flex justify-center items-center cursor-pointer',
+        styles[background], 
+        classNameProps
+    ].join(' ');
     
     return <button onClick={onClick} className={newClass} style={{ width }}>{children}</button>
 }
 
 Button.propType = {
+    background:PropTypes.string,
+    classNameProps:PropTypes.string,
+    children:PropTypes.node,
+    onClick:PropTypes.func,
     text:PropTypes.string,
     width:PropTypes.string,
-    background:PropTypes.string,
-    onClick:PropTypes.func,
-    classNameProps:PropTypes.string,
 }
