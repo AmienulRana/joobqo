25b2ad25eb01621a3d3667c0bd80819360a04de2
AmienulRana <amienulrana@gmail.com>
2022-07-12 12:06:41 +0700

    menambahkan custom class yang dikirim melalui prop

--- components/element/button/button.js
+++ components/element/button/button.js
@@ -1,7 +1,8 @@
 import styles from './button.module.scss';
 import PropTypes from 'prop-types';
-export default function Button({ children, width, background, onClick }){
-    const newClass = [styles.button, styles[background]].join(' ')
+export default function Button({ children, classNameProps, width, background, onClick }){
+    const newClass = [styles.button, styles[background], classNameProps].join(' ');
+    
     return <button onClick={onClick} className={newClass} style={{ width }}>{children}</button>
 }
 
@@ -10,4 +11,5 @@ Button.propType = {
     width:PropTypes.string,
     background:PropTypes.string,
     onClick:PropTypes.func,
+    classNameProps:PropTypes.string,
 }
