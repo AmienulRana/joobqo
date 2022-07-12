872975788f2319f7e87c870516d24bf8e7f3fd3e
AmienulRana <amienulrana@gmail.com>
2022-07-12 12:10:06 +0700

    mengubah styling module => tailwind

--- components/organism/home/tagline.js
+++ components/organism/home/tagline.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import Button from '../../element/button/button';
 import { FiSearch, GoLocation } from '../../Icons';
+import { InputIcon } from '../../element/input/input';
 import PropTypes from 'prop-types';
-import styles from './styles/tagline.module.scss';
 
 function Tagline() {
   return (
-        <section className={styles.tagline}>
-            <h1>Jobs Search for people Passionate about Startup</h1>
-            <p>Easily and quickly select thousand of jobs from around the world</p>
-            <div className={styles.searchJob}>
-            <div className={styles.wrapperInput}>
-                <input type="text" placeholder="job title"/>
-                <FiSearch />
-            </div>
-            <div className={styles.wrapperInput}>
-                <input type="text" placeholder="location"/>
-                <GoLocation />
-            </div>
-            <Button background="gradient">
-                Search
-            </Button>
+        <section>
+            <h1 className='text-5xl mt-70px font-bold mt-5'>Jobs Search for people Passionate about Startup</h1>
+            <p className="text-disabled mt-4">Easily and quickly select thousand of jobs from around the world</p>
+            <div className="flex items-center mt-5 shadow bg-white py-2.5 px-5 rounded-lg w-max h-20">
+                <InputIcon type="text" placeholder="job title" Icon={FiSearch} />
+                <InputIcon type="text" placeholder="location" Icon={GoLocation} />
+                <Button background="gradient" classNameProps="w-20 h-10">Search</Button>
             </div>
         </section>
    )
 }
 
 export default Tagline;
-Tagline.propType = {
-    styles: PropTypes.object,
-}
