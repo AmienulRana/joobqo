e56612ac44addf71089dd3ee7701d5788dc02dc9
AmienulRana <amienulrana@gmail.com>
2022-07-13 09:14:26 +0700

    mengubah styling module => tailwind

--- components/element/navbar/navbar.js
+++ components/element/navbar/navbar.js
@@ -3,16 +3,16 @@ import Link from 'next/link';
 import Button from '../button/button';
 export default function Navbar(){
     return(
-        <nav className={styles.Navbar}>
-            <h3 className={styles.Brand}>Job<span>Qoo</span></h3>
-            <ul>
-                <li className="active"><Link href="/">Home</Link></li>
-                <li><Link href="/">Jobs</Link></li>
-                <li><Link href="/">Company</Link></li>
+        <nav className="px-70px py-5 flex justify-between items-center">
+            <h3 className="text-3xl font-bold">Job<span className="text-main-color">Qoo</span></h3>
+            <ul className="flex justify-between">
+                <li className="active mr-10"><Link href="/">Home</Link></li>
+                <li className="mr-10"><Link href="/">Jobs</Link></li>
+                <li className="mr-10"><Link href="/">Company</Link></li>
             </ul>
-            <div className={styles.Authentication}>
-                <p><Link href="/">Login</Link></p>
-                <Button text="Sign up"/>
+            <div className="flex items-center">
+                <p className="mr-10"><Link href="/">Login</Link></p>
+                <Button background="gradient" width="90px">Sign up</Button>
             </div>
         </nav>
     )
