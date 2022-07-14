abf5ba2e115657b646166d5bffd9820dc6dd0870
AmienulRana <amienulrana@gmail.com>
2022-07-14 22:24:36 +0700

    menambahkan icon baru

--- components/Icons/index.js
+++ components/Icons/index.js
@@ -1,8 +1,8 @@
 import { FiSearch } from 'react-icons/fi';
 import { GoLocation } from 'react-icons/go';
 import { MdWork } from 'react-icons/md';
-import { IoClose } from 'react-icons/io5';
-import { BsLinkedin, BsGithub, BsWhatsapp } from 'react-icons/bs';
- 
-
-export { FiSearch, GoLocation, MdWork, IoClose, BsLinkedin, BsGithub, BsWhatsapp }
+import { IoClose, IoConstruct } from 'react-icons/io5';
+import { BsLinkedin, BsGithub, BsWhatsapp, BsCheckCircleFill } from 'react-icons/bs';
+import { RiComputerLine } from 'react-icons/ri';
+import { FaHotel } from 'react-icons/fa';
+export { FiSearch, GoLocation, MdWork, IoClose, BsLinkedin, BsGithub, BsWhatsapp, BsCheckCircleFill, RiComputerLine, FaHotel, IoConstruct}
