8d1e06ee3027dfe56769e74620d017c8a78ed56e
AmienulRana <amienulrana@gmail.com>
2022-07-14 13:06:37 +0700

    membuat component category

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import BannerImg from '../components/organism/home/bannerImg';
-import { BsCheckCircleFill } from '../components/Icons';
+import Category from '../components/organism/home/category';
 import Layout from '../components/layout/layout';
 import Tagline from '../components/organism/home/tagline';
 import { useState } from 'react';
 import Modal from '../components/element/modal/modal';
 import HowToApply from '../components/organism/home/howToApply';
 import SuccessHired from '../components/organism/home/successHired';
-
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
   return (
@@ -16,12 +15,15 @@ export default function Home() {
             <Tagline  />
             <BannerImg  />
           </section>
+          <section className="mt-70px">
+            <Category />
+          </section>
           <section className="mt-70px flex items-center">
             <SuccessHired setShowModal={setShowModal}/>
             <HowToApply />
           </section>
         </section>
-      <Modal showModal={showModal} setShowModal={setShowModal} />
+        <Modal showModal={showModal} setShowModal={setShowModal} />
       </Layout>
   )
 }
