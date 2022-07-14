63219493b1b99963727a5169639e7191b5dc8dcd
AmienulRana <amienulrana@gmail.com>
2022-07-14 13:07:39 +0700

    membuat komponen category dan memasukan nya kedalam organism

---
+++ components/organism/home/category/index.js
@@ -0,0 +1,16 @@
+import CardCategory from './cardCategory';
+import Link from 'next/link';
+import { RiComputerLine, IoConstruct, FaHotel } from '../../../Icons';
+export default function Category(){
+    return(
+        <>
+            <h1 className="text-3xl mb-4 font-bold">Jobs Category</h1>
+            <div className="flex justify-between items-center">
+              <CardCategory category="IT/Computer Sience" Icon={RiComputerLine} totalJobs="2500" />
+              <CardCategory category="Building/Construction" Icon={IoConstruct} totalJobs="800" />
+              <CardCategory category="Hotel/Restaurant" Icon={FaHotel} totalJobs="1200" />
+            </div>
+            <Link href="/jobs"><p className="m-auto mt-6 text-center text-main-color underline cursor-pointer">See more Jobs</p></Link>
+        </>
+    )
+}
