25a1507e495517053ef348bfa784a8ef64c6cc27
AmienulRana <amienulrana@gmail.com>
2022-07-14 21:35:11 +0700

    memperbaiki pemanggilan component input beserta props nya

--- components/element/input/input.js
+++ components/element/input/input.js
@@ -1,23 +1,24 @@
-import styles from './input.module.scss';
 import PropTypes from 'prop-types';
+
+export default function Input(props){
+    const { classNameProps} = props;
+    return(
+            <input className={classNameProps} {...props} />
+    )
+}
+
 export function InputIcon(props){
     const { Icon, type, onChange, placeholder, classNameProps, classNameIcon } = props;
     const classNameInput = ["h-full border-0 text-base pl-9", classNameProps].join(' ');
     const classIcon = ["absolute top-1/2 left-2 text-main-color font-xl -translate-y-1/2", classNameIcon].join(' ');
     return(
     <div className="relative">
-       <input type={type} placeholder={placeholder} className={classNameInput} />
+       <Input type={type} placeholder={placeholder} classNameProps={classNameInput} />
        <Icon className={classIcon} />
     </div>)
 }
-export default function Input(props){
-    const { type, placeholder } = props;
-    return(
-        <div className={styles.wrapperInput}>
-            <input type={type} placeholder={placeholder} />
-        </div>
-    )
-}
+
+
 Input.propType = {
     type: PropTypes.string,
     placeholder: PropTypes.string
