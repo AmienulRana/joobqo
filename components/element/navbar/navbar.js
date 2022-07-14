91d406c359a41b617b8a62e74f14cb5b8730013b
AmienulRana <amienulrana@gmail.com>
2022-07-14 21:44:32 +0700

    mengimport logo yang telah menjadi componen

--- components/element/navbar/navbar.js
+++ components/element/navbar/navbar.js
@@ -1,10 +1,10 @@
-import styles from './navbar.module.scss';
 import Link from 'next/link';
 import Button from '../button/button';
+import Logo from '../logo/logo';
 export default function Navbar(){
     return(
         <nav className="px-70px py-5 flex justify-between items-center">
-            <h3 className="text-3xl font-bold">Job<span className="text-main-color">Qoo</span></h3>
+            <Logo />
             <ul className="flex justify-between">
                 <li className="active mr-10"><Link href="/">Home</Link></li>
                 <li className="mr-10"><Link href="/">Jobs</Link></li>
