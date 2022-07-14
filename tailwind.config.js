cfb817d613136315dd36e38af94ee6e0dffaeb62
AmienulRana <amienulrana@gmail.com>
2022-07-14 22:26:42 +0700

    membuat konfigurasi baru di borderRadius, spacing, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [ 
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '70px': '70px',
+        '100':'100px',
+        '110': '110px',
+        '30px':'30px'
+      },
+      colors: {
+        'disabled': '#bcbcbc',
+        'main-color': '#73a0f7',
+        'orange': '#FBBC09',
+      },
+      borderRadius:{
+        'half':'50%'
+      }
+    },
   },
   plugins: [],
 }
